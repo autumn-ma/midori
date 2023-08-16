6c9d53920ba80cb1b7dc321b66ff2f81047d35de
Manish Kunwar <48282663+myan-ish@users.noreply.github.com>
2023-08-16 21:58:36 +0545

    get_page_by_url

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,24 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.serializers import ModelSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from itertools import chain
+from midori.database_router import ShardedRouter
 
 from .models import Page
+
+class ListAsQuerySet(list):
+
+    def __init__(self, *args, model, **kwargs):
+        self.model = model
+        super().__init__(*args, **kwargs)
+
+    # def filter(self, *args, **kwargs):
+    #     return self  # filter ignoring, but you can impl custom filter
+
+    def order_by(self, *args, **kwargs):
+        return self
+
 
 class PageSerializer(ModelSerializer):
     class Meta:
@@ -16,16 +33,21 @@ class PageViewSet(ModelViewSet):
         from django.db.models import Q
 
         qs_shard0 = Page.objects.using('shard0').all()
-        print(qs_shard0)
         qs_shard1 = Page.objects.using('shard1').all()
-        print(qs_shard1)
         qs_shard2 = Page.objects.using('shard2').all()
-        print(qs_shard2)
         qs_shard3 = Page.objects.using('shard3').all()
-        print(qs_shard3)
         qs_default = Page.objects.using('default').all()
-        print(qs_default)
 
-        combined_qs = qs_shard0.union(qs_shard1, qs_shard2, qs_shard3, qs_default)
-        
+        combined_qs = list(chain(qs_shard0, qs_shard1, qs_shard2, qs_shard3, qs_default))
+        combined_qs = ListAsQuerySet(combined_qs, model=Page)
         return combined_qs
+    
+    @action(detail=False, methods=['get'], url_path='get_by_url/(?P<url>.+)')
+    def get_by_url(self, request, url):
+        shard_alias = ShardedRouter().get_shard_alias(url)
+        print(url,shard_alias)
+        page = Page.objects.using(shard_alias).get(url=url)
+        serializer = PageSerializer(page)
+        return Response(serializer.data)
+
+        
